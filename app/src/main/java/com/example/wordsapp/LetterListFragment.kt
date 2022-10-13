df0e8f9e069db6a2c92a7a200cfc857644fd9616
Abdullah Mahmudov <abdullahmahmudov024@gmail.com>
2022-10-13 14:37:45 +0400

    LetterListFragment implemented

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,7 +1,83 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.*
+import com.example.wordsapp.databinding.FragmentLetterList1Binding
 
 class LetterListFragment : Fragment() {
+    private var _binding: FragmentLetterList1Binding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private var isLinear: Boolean = true
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLetterList1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.recyclerView
+        changeLayout()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+
+        val layoutButton = menu.findItem(R.id.layout)
+        setIcon(layoutButton)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.layout -> {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+                isLinear = !isLinear
+                // Sets layout and icon
+                changeLayout()
+                setIcon(item)
+
+                return true
+            }
+            //  Otherwise, do nothing and use the core event handling
+
+            // when clauses require that all possible paths be accounted for explicitly,
+            //  for instance both the true and false cases if the value is a Boolean,
+            //  or an else to catch all unhandled cases.
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun changeLayout() {
+        if (isLinear) recyclerView.layoutManager = GridLayoutManager(context, 4)
+        else recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem) {
+        if (menuItem == null) return
+        menuItem.icon = if (isLinear)
+            ContextCompat.getDrawable(this.requireContext(), R.drawable.grid)
+        else
+            ContextCompat.getDrawable(this.requireContext(), R.drawable.list)
+
+    }
 }
--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -29,61 +29,10 @@ import com.example.wordsapp.databinding.ActivityMainBinding
  * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var recyclerView: RecyclerView
-    private var isLinear: Boolean = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        recyclerView = binding.recyclerView
-        // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LetterAdapter()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        changeLayout()
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.layout -> {
-                // Sets isLinearLayoutManager (a Boolean) to the opposite value
-                isLinear= !isLinear
-                // Sets layout and icon
-                changeLayout()
-                setIcon(item)
-
-                return true
-            }
-            //  Otherwise, do nothing and use the core event handling
-
-            // when clauses require that all possible paths be accounted for explicitly,
-            //  for instance both the true and false cases if the value is a Boolean,
-            //  or an else to catch all unhandled cases.
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun changeLayout() {
-        if (isLinear) recyclerView.layoutManager = GridLayoutManager(this, 4)
-        else recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LetterAdapter()
-    }
-
-    private fun setIcon(menuItem: MenuItem) {
-        if (menuItem == null) return
-        menuItem.icon = if (isLinear)
-            ContextCompat.getDrawable(this, R.drawable.grid)
-        else
-            ContextCompat.getDrawable(this, R.drawable.list)
-
     }
 }
